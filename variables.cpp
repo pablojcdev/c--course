6c8ca1eb419c8061d18ffe7e7d0251d3d3926558
Pablo Cardozo <cardozopabloj@gmail.com>
2021-12-31 20:31:19 -0300

    Initial commit

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string character_name = "John";
+    int character_age; 
+    character_age = 35;
+    cout << "There once was a man named " << character_name << endl;
+    cout << "He was " << character_age << " years old" << endl;
+    cout << "He liked the name " << character_name << endl;
+    cout << "But did not like being " << character_age << endl;
+
+    return 0;
+}
